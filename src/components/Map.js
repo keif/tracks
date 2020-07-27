86ac8f00e11d0c7c8529bb59e4cd3edf1e3ca24a
Keith T. Baker <hello@keif.name>
2020-07-27 01:32:23 -0400

    working on track create

---
+++ src/components/Map.js
@@ -0,0 +1,15 @@
+import React from "react"
+import { StyleSheet, View } from "react-native"
+import { Text } from "react-native-elements"
+
+const Map = () => {
+    return (
+        <View>
+            <Text>I am a map</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({})
+
+export default Map
